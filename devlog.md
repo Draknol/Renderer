3815f923037908ed91bf29ffaf7a86d392236ae2
Draknol <reubenthomas.rr@gmail.com>
2025-05-28 22:56:14 +1200

    Flat color window added

--- devlog.md
+++ devlog.md
@@ -19,4 +19,11 @@ int main() {
 }
 ```
 ### goal:
-My eventual goal for this project it to make a simple 3D renderer with textures, file loading (eg .obj), camera controls and hopefully some lighting.
+My eventual goal for this project is to make a simple 3D renderer with textures, file loading (eg .obj), camera controls and hopefully some lighting.
+## Version 0.0.1
+### description:
+I've added a GLFWwindow with a plain blue background, resizing (not visible on blank window) using a callback, which works similar to sfml's event system, and input processing (escape closes window).
+I found a good tutorial at ()[learnopengl.com] which covers most of the basics of OpenGL with clear explanations which has been a big help.
+I decided to use OpenGL 3.3 as it is a common starting point and later versions mostly only add features beyond the scope of this project.
+### goal:
+Next I'm planning to get to the stage of rendering a triangle and by extension, other simple 2D geometry. I have a decent understanding of vertex arrays from sfml but it will be interesting getting to use shaders which I haven't used for anything before.
--- main.cpp
+++ main.cpp
@@ -6,8 +6,74 @@
 #include <GL/glu.h>
 #include <GLFW/glfw3.h>
 
-int main() {
-    std::cout << "Hello World!";
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
 
+void processInput(GLFWwindow *window);
+
+// Callbacks
+void windowResized(GLFWwindow* window, int width, int height);
+
+int main() {
+
+    // Initialise glfw
+    glfwInit();
+
+    // Set version to 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create window
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Renderer", NULL, NULL);
+
+    // Check window was created
+    if (!window) {
+        std::cout << "Failed to create window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    // Set window as main window
+    glfwMakeContextCurrent(window);
+
+    // Disable VSync
+    glfwSwapInterval(0);
+
+    // Create a viewport
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    // Set function to be called on size change
+    glfwSetFramebufferSizeCallback(window, windowResized);
+
+    // Set clear color
+    glClearColor(0.5f, 0.65f, 0.85f, 1.0f);
+    
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+
+        processInput(window);
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    // Exit
+    glfwTerminate();
     return 0;
+}
+
+void processInput(GLFWwindow *window) {
+    // Escape
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void windowResized(GLFWwindow* window, int width, int height) {
+    // Recreate viewport
+    glViewport(0, 0, width, height);
 }
