12c2ee98cacc3f428484df1914751dabae3ea851
Draknol <reubenthomas.rr@gmail.com>
2025-05-31 21:37:20 +1200

    Simple 2D geometry added

--- devlog.md
+++ devlog.md
@@ -27,3 +27,8 @@ I found a good tutorial at ()[learnopengl.com] which covers most of the basics o
 I decided to use OpenGL 3.3 as it is a common starting point and later versions mostly only add features beyond the scope of this project.
 ### goal:
 Next I'm planning to get to the stage of rendering a triangle and by extension, other simple 2D geometry. I have a decent understanding of vertex arrays from sfml but it will be interesting getting to use shaders which I haven't used for anything before.
+## Version 0.0.2
+### description:
+Rendering of 2D geometry is now possible. I have tested triangles, squares and messed around with circles. Even at 10 million vertices my PC was still able to render at ~30fps which while I imagine that will decrease as I add more complexity I am definitely getting a feel for the efficiency of OpenGL. I have learnt about VAOs (Vertex Array Objects), which are very effectively the same as vertex arrays in sfml, VBOs (Vertex Buffer Objects), which allow batching vertices together to improve efficiency of sending information to the GPU, and touched on shaders, compiling a very simple vertex and fragment shader as a placeholder. Shaders were something I hadn't made use of in sfml so I am looking forward to gaining a better understanding of how they work. The tutorial also covers EBOs (Element Buffer Objects), which are arrays of indices that allow for reuse of vertices. I haven't implemented an EBO yet although they will likely be very useful once I get to more complex geometry.
+### goal:
+Next I'll be making some more interesting shaders (the current ones are placeholders) and learning some GLSL.
--- main.cpp
+++ main.cpp
@@ -6,10 +6,42 @@
 #include <GL/glu.h>
 #include <GLFW/glfw3.h>
 
-const int SCREEN_WIDTH = 1280;
-const int SCREEN_HEIGHT = 720;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 800;
+
+const char* vertexShaderSource = (
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0"
+);
+
+const char* fragmentShaderSource = (
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0"
+);
 
 void processInput(GLFWwindow *window);
+
+// Simple 2D geometry (square with a hole)
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+    -0.25f, -0.25f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.25f, -0.25f, 0.0f,
+    0.5f, 0.5f, 0.0f,
+    0.25f, 0.25f, 0.0f,
+    -0.5f, 0.5f, 0.0f,
+    -0.25f, 0.25f, 0.0f,
+    -0.5f, -0.5f, 0.0f,
+    -0.25f, -0.25f, 0.0f
+};
 
 // Callbacks
 void windowResized(GLFWwindow* window, int width, int height);
@@ -36,6 +68,7 @@ int main() {
 
     // Set window as main window
     glfwMakeContextCurrent(window);
+    glewInit();
 
     // Disable VSync
     glfwSwapInterval(0);
@@ -48,6 +81,50 @@ int main() {
 
     // Set clear color
     glClearColor(0.5f, 0.65f, 0.85f, 1.0f);
+
+    // Create and bind VBO
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Create shaders
+    GLuint vertexShader;
+    GLuint fragmentShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    // Compile shaders
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(vertexShader);
+    glCompileShader(fragmentShader);
+
+    // Create shader program
+    GLuint shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    // Attach shaders to program
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Delete linked shaders
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Create and bind VAO
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    
+    // Copy array to buffer
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Apply attributes to VBO
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
     
     // Main loop
     while (!glfwWindowShouldClose(window)) {
@@ -55,6 +132,12 @@ int main() {
         processInput(window);
 
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        
+        glBindVertexArray(VAO);
+
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, sizeof(vertices) / sizeof(float));
 
         glfwSwapBuffers(window);
 
